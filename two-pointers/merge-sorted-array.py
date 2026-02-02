9d3e714b7f47994d4d35e6475f44e5b4501b7f8e
Hasinur Rahman <hasinurrahman3@gmail.com>
2026-02-02 11:24:57 +0600

    new: Added merge sorted array solution

---
+++ two-pointers/merge-sorted-array.py
@@ -0,0 +1,30 @@
+class Solution:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        p1, p2 = m - 1, n - 1
+        p = m + n - 1
+
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
+
+        # If there are remaining elements in nums2, copy them
+        while p2 >= 0:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+            p -= 1
+
+# Example usage:
+# nums1 = [1,2,3,0,0,0]
+# m = 3
+# nums2 = [2,5,6]
+# n = 3
+# sol = Solution()
+# sol.merge(nums1, m, nums2, n)
